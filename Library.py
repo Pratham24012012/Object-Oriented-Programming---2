fb225d56453576e23d45ad897f8b8d8cd814f3e3
Pratham24012012 <prathviksolanki@gmail.com>
2026-02-19 17:59:43 +0400

    hi

--- Library.py
+++ Library.py
@@ -23,4 +23,33 @@ class Library:
 if __name__=='__main__':
     L=['Python for Kids', "Computer Science", "Harry Potter", "Magic of thinking big", "Rich Dad Poor Dad", "Java"]
     #Pride & Prejudice 
-    
+    o1=Library(L, "Let's Upgrow")#creating object of Library
+while (True):
+    print("Welcome to the", o1.name, "library. Enter your choice to continue...")
+    print("1. Display books")
+    print("2. Lend a book")
+    print("3. Add a book")
+    print("4. Return a book")
+    print("5. Exit")
+    choice=int(input("Enter your choice: "))
+    if choice not in [1,2,3,4,5]:
+        print("Enter a valid choice")
+        continue#loop will start again
+    if choice==1:
+        o1.disp()
+    elif choice==2:
+        uname=input("Enter your name: ")
+        bname=input("Enter book title: ")
+        o1.Lbook(uname, bname)
+    elif choice==3:
+        bname=input("Enter book title: ") #Atomic habits
+        o1.abook(bname)
+    elif choice==4:
+       bname=input("Enter book title to be returned : ")
+       o1.rbook(bname)
+    else:
+       choice2=input("Do you really want to exit (y / n)?")
+    if choice2=='n':
+        continue
+    else:
+        exit()
